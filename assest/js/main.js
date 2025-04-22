6b1afb8dd37923fe4b5019a2d8e90a852a79b927
masadii <githubmasadi@gmail.com>
2025-04-22 14:28:07 +0700

    Tampilan home sudah selesai serta menambahkan favicon

---
+++ assest/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const topupBtn = document.getElementById('topupbtn')
+    const beliBtn = document.getElementById('beli')
+
+    topupBtn.addEventListener('click', () => {
+        window.location.href = 'topup.html'
+    })
+
+    beliBtn.addEventListener('click', () => {
+        window.location.href = 'topup.html'
+    })
+})
